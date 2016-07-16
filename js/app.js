229e25edfc823b3332e1a128b71bdb3ef90bd889
ron93 <ronmkare1@gmail.com>
2016-07-16 15:39:03 +0300

    donate

--- js/app.js
+++ js/app.js
@@ -1,21 +1,5 @@
 $(document).foundation()
-var show = document.getElementById("slider");
 
-//image array
-var pics = ["url('../img/aids.jpeg')", "url('../img/hope.jpeg')", "url('../img/hunger.jpeg')", "url('../img/thanks.jpeg')"
 
-var counter = 0;
 
-function slider() {
-    
-  
-    show.style.backgroundImage = pics[counter];
-    counter++;
-    //counter reset
-    if (counter >= pics.length) {
-        counter = 0;
-    }
-   
-}
-//interval
-setInterval(slider, 3000);
+
