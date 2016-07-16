f042503acc66eaf02689e18615b63984156226c5
ron93 <ronmkare1@gmail.com>
2016-07-16 12:51:38 +0300

    contacts

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 $(document).foundation()
-var blue = document.getElementById("main-slider");
+var show = document.getElementById("slider");
 
 //image array
-var pics = ["url(../img/aids.jpeg)", "url('../img/hope.jpeg')", "url('../img/hunger.jpeg')", "url('../img/thanks.jpeg')"
+var pics = ["url('../img/aids.jpeg')", "url('../img/hope.jpeg')", "url('../img/hunger.jpeg')", "url('../img/thanks.jpeg')"
 
 var counter = 0;
 
 function slider() {
     
   
-    blue.style.backgroundImage = pics[counter];
+    show.style.backgroundImage = pics[counter];
     counter++;
     //counter reset
     if (counter >= pics.length) {
