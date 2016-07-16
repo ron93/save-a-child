672c8867fb6ebb278f5b34b73e30884b45c3664b
ron93 <ronmkare1@gmail.com>
2016-07-16 12:28:04 +0300

    about and js slider

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 $(document).foundation()
+var blue = document.getElementById("main-slider");
+
+//image array
+var pics = ["url(../img/aids.jpeg)", "url('../img/hope.jpeg')", "url('../img/hunger.jpeg')", "url('../img/thanks.jpeg')"
+
+var counter = 0;
+
+function slider() {
+    
+  
+    blue.style.backgroundImage = pics[counter];
+    counter++;
+    //counter reset
+    if (counter >= pics.length) {
+        counter = 0;
+    }
+   
+}
+//interval
+setInterval(slider, 3000);
